a95a025642ba95c16f6af15f5070860c013d2645
Yvan2Ero <yvankanaj@gmail.com>
2022-06-16 09:14:11 +0100

    Retouche de l'ecran de commentaires

--- SportHighligths/src/services/auth.ts
+++ SportHighligths/src/services/auth.ts
@@ -6,12 +6,10 @@ export const BASE_URL = 'http://192.168.118.69:8000'
 export const http = axios.create({ baseURL: BASE_URL })
 
 const requestRefresh: TokenRefreshRequest = async (refreshToken: string): Promise<string> => {
-
     // Important! Do NOT use the axios instance that you supplied to applyAuthTokenInterceptor
     // because this will result in an infinite loop when trying to refresh the token.
     // Use the global axios client or a different instance
     const response = await axios.post(`${BASE_URL}/auth/refresh`, { token: refreshToken })
-
     return response.data.access
 }
 
